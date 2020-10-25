31262f1a8571dfc18860292a58e0d1a82dec3da8
thirdmadman <thirdmadman@gmail.com>
2020-10-25 20:17:07 +0400

    init: impl function of towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let finalArray = [];
+    matrix ?  matrix.forEach((e,i) => e.length > 0 ? (!(i % 2) ? e.forEach(b => finalArray.push(b)) : e.reverse().forEach(c => finalArray.push(c))) : console.log("gg")) : "";
+    return finalArray;
 }
